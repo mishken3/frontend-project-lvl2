effd5c2b9884ebbef0e099cefbe1529a1888bf9e
Mikhail <mishken3@gmail.com>
2022-03-12 00:30:44 +0300

    setup changeDetector.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
-import gendif from '../src/gendiff.js';
+import { Command } from 'commander';
 
-console.log('test');
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --forman <type>', 'output format');
+
+program.parse();

9850b94011756daf8a3613f2a08740cd15df3d36
Mikhail <mishken3@gmail.com>
2022-03-21 16:32:16 +0300

    add test for yaml compair

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,16 +1,26 @@
 import genDiff from '../src/genDiff.js';
-import { convertJSON, convertTXT } from '../src/convertFiles.js';
+import parser from '../src/parsers.js';
 
 let firstFlatJSON;
 let secondFlatJSON;
-let expetedFlatJSON;
+let expetedFlatResult;
+let firstFlatYAML;
+let secondFlatYAML;
 
 beforeAll(() => {
-  firstFlatJSON = convertJSON('flat_json1.json');
-  secondFlatJSON = convertJSON('flat_json2.json');
-  expetedFlatJSON = convertTXT('expected_flat_json.txt');
+  expetedFlatResult = parser('expected_flat_result.txt');
+
+  firstFlatJSON = parser('flat_json1.json');
+  secondFlatJSON = parser('flat_json2.json');
+
+  firstFlatYAML = parser('flat_yaml1.yaml');
+  secondFlatYAML = parser('flat_yaml2.yaml');
 });
 
-test('basic work', () => {
-  expect(genDiff(firstFlatJSON, secondFlatJSON)).toBe(expetedFlatJSON);
+test('Flat JSON', () => {
+  expect(genDiff(firstFlatJSON, secondFlatJSON)).toBe(expetedFlatResult);
+});
+
+test('Flat YAML', () => {
+  expect(genDiff(firstFlatYAML, secondFlatYAML)).toBe(expetedFlatResult);
 });

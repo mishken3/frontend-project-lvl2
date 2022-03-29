70f39f83a0c34b6ed5a8382d067d13f5a72f2b5f
Mikhail <mishken3@gmail.com>
2022-03-29 14:21:12 +0300

    added buildDiffTree to own file

---
+++ src/buildDiffTree.js
@@ -0,0 +1,59 @@
+import _ from 'lodash';
+
+const buildDiffTree = (obj1, obj2) => {
+  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
+  const sortedKeys = _.sortBy(keys);
+
+  const checkDiff = (key) => {
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      return {
+        type: 'nested',
+        key,
+        children: buildDiffTree(obj1[key], obj2[key]),
+      };
+    }
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
+      // deleted
+      return {
+        type: 'deleted',
+        key,
+        value: obj1[key],
+        // `  - ${key}: ${obj1[key]}`;
+      };
+    }
+    if (!_.has(obj1, key) && _.has(obj2, key)) {
+      // added
+      return {
+        type: 'added',
+        key,
+        value: obj2[key],
+        // `  + ${key}: ${obj2[key]}`;
+      };
+    }
+    if (obj1[key] !== obj2[key]) {
+      // changed
+      return {
+        type: 'changed',
+        key,
+        value1: obj1[key],
+        value2: obj2[key],
+        // `  - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}`;
+      };
+    }
+    if (obj1[key] === obj2[key]) {
+      // unchanged
+      return {
+        type: 'unchanged',
+        key,
+        value: obj1[key],
+        // `    ${key}: ${obj1[key]}`;
+      };
+    }
+
+    return null;
+  };
+
+  return sortedKeys.map(checkDiff);
+};
+
+export default buildDiffTree;
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,66 +1,11 @@
-import _ from 'lodash';
 import { extname } from 'path';
 import * as fs from 'fs';
 import parser from './parsers.js';
 import stylish from './stylish.js';
+import buildDiffTree from './buildDiffTree.js';
 
-const getFileFormat = (filepath) => extname(filepath).toLocaleLowerCase().split('.')[1];
-
-const buildDiffTree = (obj1, obj2) => {
-  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-  const sortedKeys = _.sortBy(keys);
-
-  const checkDiff = (key) => {
-    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      return {
-        type: 'nested',
-        key,
-        children: buildDiffTree(obj1[key], obj2[key]),
-      };
-    }
-    if (_.has(obj1, key) && !_.has(obj2, key)) {
-      // deleted
-      return {
-        type: 'deleted',
-        key,
-        value: obj1[key],
-        // `  - ${key}: ${obj1[key]}`;
-      };
-    }
-    if (!_.has(obj1, key) && _.has(obj2, key)) {
-      // added
-      return {
-        type: 'added',
-        key,
-        value: obj2[key],
-        // `  + ${key}: ${obj2[key]}`;
-      };
-    }
-    if (obj1[key] !== obj2[key]) {
-      // changed
-      return {
-        type: 'changed',
-        key,
-        value1: obj1[key],
-        value2: obj2[key],
-        // `  - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}`;
-      };
-    }
-    if (obj1[key] === obj2[key]) {
-      // unchanged
-      return {
-        type: 'unchanged',
-        key,
-        value: obj1[key],
-        // `    ${key}: ${obj1[key]}`;
-      };
-    }
-
-    return null;
-  };
-
-  return sortedKeys.map(checkDiff);
-};
+const getFileFormat = (filepath) =>
+  extname(filepath).toLocaleLowerCase().split('.')[1];
 
 function genDiff(filepath1, filepath2, format = 'stylish') {
   const data1 = fs.readFileSync(filepath1, 'utf-8');

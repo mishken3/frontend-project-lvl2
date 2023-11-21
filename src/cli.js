a43fb5f0ce7f3bb00547cd4aa317032e4c034ee2
Mikhail Burtsev <mishken3@gmail.com>
2023-11-21 23:42:09 +0300

    step one

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+export default program;

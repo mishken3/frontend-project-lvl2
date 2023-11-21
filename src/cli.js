000521438f0e8500ff68f30bf86f20781e83f021
Mikhail Burtsev <mishken3@gmail.com>
2023-11-22 00:17:38 +0300

    added arguments & format option

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,13 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
+
+program.parse(process.argv);
+
+// TODO: Needs later
+// const options = program.opts();
 
 export default program;

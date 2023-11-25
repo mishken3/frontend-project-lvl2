33c1e91d7abe5196fbfb107203c8e7e705a5c5ef
Mikhail Burtsev <mishken3@gmail.com>
2023-11-26 00:57:07 +0300

    splitted logic for different parsers

--- src/utils/__tests__/parse.test.js
+++ src/utils/__tests__/parse.test.js
@@ -1,7 +1,11 @@
-import parse from '../parse.js';
+import { test, expect, describe } from '@jest/globals';
+import parser from '../../parser/parser.js';
 
-const filepath1 = './fixtures/file1.json';
-const filepath2 = './fixtures/file2.json';
+const filepath1JSON = './fixtures/file1.json';
+const filepath2JSON = './fixtures/file2.json';
+
+const filepath1YAML = './fixtures/file1.yaml';
+const filepath2YAML = './fixtures/file2.yaml';
 
 const parsedFile1 = {
   host: 'hexlet.io',
@@ -16,10 +20,22 @@ const parsedFile2 = {
   host: 'hexlet.io',
 };
 
-test('parsing first filepath', () => {
-  expect(parse(filepath1)).toStrictEqual(parsedFile1);
+describe('JSON extension', () => {
+  test('parsing first filepath', () => {
+    expect(parser(filepath1JSON)).toStrictEqual(parsedFile1);
+  });
+
+  test('parsing second filepath', () => {
+    expect(parser(filepath2JSON)).toStrictEqual(parsedFile2);
+  });
 });
 
-test('parsing second filepath', () => {
-  expect(parse(filepath2)).toStrictEqual(parsedFile2);
+describe('YAML extension', () => {
+  test('parsing first filepath', () => {
+    expect(parser(filepath1YAML)).toStrictEqual(parsedFile1);
+  });
+
+  test('parsing second filepath', () => {
+    expect(parser(filepath2YAML)).toStrictEqual(parsedFile2);
+  });
 });

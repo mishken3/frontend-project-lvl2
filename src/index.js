49679ba6e98441f588e96aa3c6463fa78bb60ab8
Mikhail Burtsev <mishken3@gmail.com>
2023-11-25 21:49:33 +0300

    jest in package & pre-hook& new lint config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import parse from './utils/parse.js';
 import { compare } from './utils/compare.js';
 
+// eslint-disable-next-line no-unused-vars
 const genDiff = (filepath1, filepath2, config = {}) => {
   const file1 = parse(filepath1);
   const file2 = parse(filepath2);

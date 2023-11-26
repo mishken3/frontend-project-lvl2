bde9d7c596cdda7547662a6d7f3d25c821393841
Mikhail Burtsev <mishken3@gmail.com>
2023-11-26 15:50:51 +0300

    remowrk tests&fixtures

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,18 @@
+import { expect, test } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFileSync } from 'fs';
+
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) =>
+  join(__dirname, '..', '__fixtures__', filename);
+
+test('stylish format', () => {
+  expect(
+    genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')),
+  ).toBe(readFileSync(getFixturePath('stylish.txt'), 'utf-8'));
+});
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-const DEFAULT_TAB_SIZE = 4;
-export const DEFAULT_TAB_SPACE = ' '.repeat(DEFAULT_TAB_SIZE);

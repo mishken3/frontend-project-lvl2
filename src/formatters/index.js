55358ef4eda8c30ecdcaeba3d6a691b7006f4529
Mikhail <mishken3@gmail.com>
2023-12-11 02:15:46 +0300

    json formatter implement

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,14 +1,20 @@
 import { stylish } from './stylish.js';
 import { plain } from './plain.js';
+import { json } from './json.js';
 
 const FORMATTING_TYPES = {
   plain: 'plain',
   stylish: 'stylish',
+  json: 'json',
 };
 
 const formatter = (diff, format) => {
   if (format === FORMATTING_TYPES.plain) {
     return plain(diff);
+  }
+
+  if (format === FORMATTING_TYPES.json) {
+    return json(diff);
   }
 
   return stylish(diff);
---
+++ src/formatters/json.js
@@ -0,0 +1,1 @@
+export const json = (diffAst) => JSON.stringify(diffAst, '', 2);
